9c0d5df53b2a61429abdacfe1b947c282d9e397d
OdintsovMikhail <mihail.odincov@student.ehu.lt>
2026-05-12 23:27:52 +0300

    tests fix

--- resolvers.py
+++ resolvers.py
@@ -1,5 +1,6 @@
 from utility import get_connection, DB_SCHEMA
 from gql_types import User
+import strawberry
 import logging
 
 logger = logging.getLogger("user_service")
@@ -8,12 +9,14 @@ S = DB_SCHEMA
 
 def resolve_user_by_id(id: int) -> User:
     with get_connection() as conn:
-        row = conn.cursor().execute(
+        cursor = conn.cursor()
+        cursor.execute(
             f"SELECT Id, Username, Email FROM [{S}].[User] WHERE Id = ?", id
-        ).fetchone()
+        )
+        row = cursor.fetchone()
 
     if not row:
-        raise ValueError(f"User with id {id} not found")
+        raise strawberry.exceptions.GraphQLError(f"User with id {id} not found")
 
     logger.info("User with id %s found", id)
     return User(id=row[0], username=row[1], email=row[2])
@@ -21,12 +24,14 @@ def resolve_user_by_id(id: int) -> User:
 
 def resolve_user_by_username(username: str) -> User:
     with get_connection() as conn:
-        row = conn.cursor().execute(
+        cursor = conn.cursor()
+        cursor.execute(
             f"SELECT Id, Username, Email FROM [{S}].[User] WHERE Username = ?", username
-        ).fetchone()
+        )
+        row = cursor.fetchone()
 
     if not row:
-        raise ValueError(f"User '{username}' not found")
+        raise strawberry.exceptions.GraphQLError(f"User '{username}' not found")
 
     logger.info("User with username %s found id=%s", username, row[0])
     return User(id=row[0], username=row[1], email=row[2])
